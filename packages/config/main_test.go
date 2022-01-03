fc92943ecbc04cf6e2f3ac982c905c2d1859e43d
doppeldenken <eduardoaemunoz@gmail.com>
2022-01-03 22:58:40 +0000

    fix tests

--- packages/config/main_test.go
+++ packages/config/main_test.go
@@ -9,7 +9,7 @@ import (
 
 func init() {
 	_, filename, _, _ := runtime.Caller(0)
-	dir := path.Join(path.Dir(filename), "../")
+	dir := path.Join(path.Dir(filename), "../..")
 	err := os.Chdir(dir)
 	if err != nil {
 		panic(err)
@@ -17,11 +17,21 @@ func init() {
 }
 
 func TestLocalConfig(t *testing.T) {
-	tests := []Config{{"imsosecret", []string{"secrets.geheim.yaml"}, true, true}}
+	tests := []Config{{"imsosecret", []string{
+		"testfiles/config.json",
+		"testfiles/coverage_testfile.xml",
+		"testfiles/helpers.sh",
+		"testfiles/id_rsa",
+		"testfiles/id_rsa.pub",
+		"testfiles/known_hosts",
+		"testfiles/secrets_test.geheim.yaml",
+		"testfiles/simple.txt",
+		"testfiles/supervisor_env",
+	}, true, false}}
 	for _, test := range tests {
 		config := Get()
 		if !compareConfigs(config, test) {
-			t.Errorf("Get() == %s, expected %s", config, test)
+			t.Errorf("Get() == %v, expected %v", config, test)
 		}
 	}
 }
--- packages/config/types_test.go
+++ packages/config/types_test.go
@@ -10,17 +10,17 @@ type CompareConfigsCases struct {
 
 func TestCompareConfigs(t *testing.T) {
 	tests := []CompareConfigsCases{
-		{Config{"imsecret", []string{".geheim/config.yaml"}, true, true}, Config{"imsecret", []string{".geheim/config.yaml"}, true, true}, true},
-		{Config{"imsecret", []string{".geheim/config.yaml"}, true, true}, Config{"imnot", []string{".geheim/config.yaml"}, true, true}, false},
-		{Config{"imsecret", []string{".geheim/config.yaml"}, true, true}, Config{"imsecret", []string{"~/geheim/config.yaml"}, true, true}, false},
-		{Config{"", []string{".geheim/config.yaml"}, true, true}, Config{"imsecret", []string{".geheim/config.yaml"}, true, true}, true},
-		{Config{"imsecret", []string{}, true, true}, Config{"imsecret", []string{".geheim/config.yaml"}, true, true}, true},
-		{Config{"imsecret", []string{".geheim/config.yaml", "~/geheim/config.yaml"}, true, true}, Config{"imsecret", []string{".geheim/config.yaml"}, true, true}, false},
+		{Config{"imsosecret", []string{".geheim/config.yaml"}, true, true}, Config{"imsosecret", []string{".geheim/config.yaml"}, true, true}, true},
+		{Config{"imsosecret", []string{".geheim/config.yaml"}, true, true}, Config{"imnot", []string{".geheim/config.yaml"}, true, true}, false},
+		{Config{"imsosecret", []string{".geheim/config.yaml"}, true, true}, Config{"imsosecret", []string{"~/geheim/config.yaml"}, true, true}, false},
+		{Config{"", []string{".geheim/config.yaml"}, true, true}, Config{"imsosecret", []string{".geheim/config.yaml"}, true, true}, false},
+		{Config{"imsosecret", []string{}, true, true}, Config{"imsosecret", []string{".geheim/config.yaml"}, true, true}, false},
+		{Config{"imsosecret", []string{".geheim/config.yaml", "~/geheim/config.yaml"}, true, true}, Config{"imsosecret", []string{".geheim/config.yaml"}, true, true}, false},
 	}
 	for _, test := range tests {
 		r := compareConfigs(test.A, test.B)
 		if test.Expected != r {
-			t.Errorf("compareConfigs(%s, %s) == %t, expected %t", test.A, test.B, r, test.Expected)
+			t.Errorf("compareConfigs(%v, %v) == %t, expected %t", test.A, test.B, r, test.Expected)
 		}
 	}
 }
