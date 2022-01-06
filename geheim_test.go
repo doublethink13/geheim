0d02830c5579ef0dc69afd87d93b1238f6c1b0a4
doppeldenken <eduardoaemunoz@gmail.com>
2022-01-06 19:27:40 +0000

    continuing testing geheim

--- geheim_test.go
+++ geheim_test.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"testing"
+	"treuzedev/geheim/packages/config"
 	"treuzedev/geheim/testhelpers"
 )
 
 func TestGeheim(t *testing.T) {
 	tests := testhelpers.TestCases
 	for _, test := range tests {
+		generateTestFiles(test.Config)
 		t.Run(test.Name, func(t *testing.T) {
 			geheim(test.Config)
+			checkTestfileResult(test.Config)
 		})
+		removeTestFiles(test.Config)
 	}
+}
+
+func generateTestFiles(config config.Config) {
+	return
+}
+
+func checkTestfileResult(config config.Config) {
+	return
+}
+
+func removeTestFiles(config config.Config) {
+	return
 }
--- testhelpers/test_cases.go
+++ testhelpers/test_cases.go
@@ -5,62 +5,102 @@ import "treuzedev/geheim/packages/config"
 var TestCases = []struct {
 	Name     string
 	Config   config.Config
+	Testfile string
 	Expected string
 }{
-	{Name: "encrypt decrypted file", Config: config.Config{
-		Check:     "",
-		SecretKey: "imsosecret",
-		Encrypt:   true,
-		Decrypt:   false,
-		Files:     []string{"testfile1.test"},
-	}, Expected: ""},
-	{Name: "decrypt encrypted file", Config: config.Config{
-		Check:     "",
-		SecretKey: "imsosecret",
-		Encrypt:   false,
-		Decrypt:   true,
-		Files:     []string{"testfile2.test"},
-	}, Expected: ""},
-	{Name: "encrypt encrypted file", Config: config.Config{
-		Check:     "",
-		SecretKey: "imsosecret",
-		Encrypt:   true,
-		Decrypt:   false,
-		Files:     []string{"testfile1.test"},
-	}, Expected: ""},
-	{Name: "decrypt decrypted file", Config: config.Config{
-		Check:     "",
-		SecretKey: "imsosecret",
-		Encrypt:   false,
-		Decrypt:   true,
-		Files:     []string{"testfile2.test"},
-	}, Expected: ""},
-	{Name: "check encrypted file that is encrypted", Config: config.Config{
-		Check:     "",
-		SecretKey: "imsosecret",
-		Encrypt:   true,
-		Decrypt:   false,
-		Files:     []string{"testfile3.test"},
-	}, Expected: ""},
-	{Name: "check decrypted file that is decrypted", Config: config.Config{
-		Check:     "",
-		SecretKey: "imsosecret",
-		Encrypt:   true,
-		Decrypt:   false,
-		Files:     []string{"testfile4.test"},
-	}, Expected: ""},
-	{Name: "check encrypted file that is decrypted", Config: config.Config{
-		Check:     "",
-		SecretKey: "imsosecret",
-		Encrypt:   true,
-		Decrypt:   false,
-		Files:     []string{"testfile5.test"},
-	}, Expected: ""},
-	{Name: "check decrypted file that is encrypted", Config: config.Config{
-		Check:     "",
-		SecretKey: "imsosecret",
-		Encrypt:   true,
-		Decrypt:   false,
-		Files:     []string{"testfile6.test"},
-	}, Expected: ""},
+	{
+		Name: "encrypt decrypted file",
+		Config: config.Config{
+			Check:     "",
+			SecretKey: "imsosecret",
+			Encrypt:   true,
+			Decrypt:   false,
+			Files:     []string{"testfile1.test"},
+		},
+		Testfile: "",
+		Expected: "",
+	},
+	{
+		Name: "decrypt encrypted file",
+		Config: config.Config{
+			Check:     "",
+			SecretKey: "imsosecret",
+			Encrypt:   false,
+			Decrypt:   true,
+			Files:     []string{"testfile2.test"},
+		},
+		Testfile: "",
+		Expected: "",
+	},
+	{
+		Name: "encrypt encrypted file",
+		Config: config.Config{
+			Check:     "",
+			SecretKey: "imsosecret",
+			Encrypt:   true,
+			Decrypt:   false,
+			Files:     []string{"testfile1.test"},
+		},
+		Testfile: "",
+		Expected: "",
+	},
+	{
+		Name: "decrypt decrypted file",
+		Config: config.Config{
+			Check:     "",
+			SecretKey: "imsosecret",
+			Encrypt:   false,
+			Decrypt:   true,
+			Files:     []string{"testfile2.test"},
+		},
+		Testfile: "",
+		Expected: "",
+	},
+	{
+		Name: "check encrypted file that is encrypted",
+		Config: config.Config{
+			Check:     "",
+			SecretKey: "imsosecret",
+			Encrypt:   true,
+			Decrypt:   false,
+			Files:     []string{"testfile3.test"},
+		},
+		Testfile: "",
+		Expected: "",
+	},
+	{
+		Name: "check decrypted file that is decrypted",
+		Config: config.Config{
+			Check:     "",
+			SecretKey: "imsosecret",
+			Encrypt:   true,
+			Decrypt:   false,
+			Files:     []string{"testfile4.test"},
+		},
+		Testfile: "",
+		Expected: ""},
+	{
+		Name: "check encrypted file that is decrypted",
+		Config: config.Config{
+			Check:     "",
+			SecretKey: "imsosecret",
+			Encrypt:   true,
+			Decrypt:   false,
+			Files:     []string{"testfile5.test"},
+		},
+		Testfile: "",
+		Expected: "",
+	},
+	{
+		Name: "check decrypted file that is encrypted",
+		Config: config.Config{
+			Check:     "",
+			SecretKey: "imsosecret",
+			Encrypt:   true,
+			Decrypt:   false,
+			Files:     []string{"testfile6.test"},
+		},
+		Testfile: "",
+		Expected: "",
+	},
 }
