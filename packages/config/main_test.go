f1d926f893f1ee3e3a601114f86559b132a7aa97
doppeldenken <eduardoaemunoz@gmail.com>
2022-01-11 21:12:59 +0000

    check encryption done; also fixed short/long flag usage

--- packages/config/main_test.go
+++ packages/config/main_test.go
@@ -1,37 +1,136 @@
 package config
 
 import (
+	"flag"
+	"fmt"
 	"os"
-	"path"
-	"runtime"
 	"testing"
 )
 
-func init() {
-	_, filename, _, _ := runtime.Caller(0)
-	dir := path.Join(path.Dir(filename), "../..")
-	err := os.Chdir(dir)
-	if err != nil {
-		panic(err)
+type flags struct {
+	check     []string
+	secretKey []string
+	encrypted string
+	decrypted string
+}
+
+var testCheckEncryptedCases = []struct {
+	name     string
+	flags    flags
+	expected Config
+}{
+	{
+		name: "short flag + short option",
+		flags: flags{
+			check:     []string{"-c", "e"},
+			secretKey: []string{"-k", "test"},
+			encrypted: "",
+			decrypted: "",
+		},
+		expected: Config{
+			Check:     "e",
+			SecretKey: "test",
+			Encrypt:   true,
+			Decrypt:   false,
+			Files:     []string{"secrets.geheim.yaml"},
+		},
+	},
+	{
+		name: "short flag + long option",
+		flags: flags{
+			check:     []string{"-c", "encrypted"},
+			secretKey: []string{"-k", "test"},
+			encrypted: "",
+			decrypted: "",
+		},
+		expected: Config{
+			Check:     "encrypted",
+			SecretKey: "test",
+			Encrypt:   true,
+			Decrypt:   false,
+			Files:     []string{"secrets.geheim.yaml"},
+		},
+	},
+	{
+		name: "long flag + short option",
+		flags: flags{
+			check:     []string{"--check", "e"},
+			secretKey: []string{"-k", "test"},
+			encrypted: "",
+			decrypted: "",
+		},
+		expected: Config{
+			Check:     "e",
+			SecretKey: "test",
+			Encrypt:   true,
+			Decrypt:   false,
+			Files:     []string{"secrets.geheim.yaml"},
+		},
+	},
+	{
+		name: "long flag + long option",
+		flags: flags{
+			check:     []string{"--check", "encrypted"},
+			secretKey: []string{"-k", "test"},
+			encrypted: "",
+			decrypted: "",
+		},
+		expected: Config{
+			Check:     "encrypted",
+			SecretKey: "test",
+			Encrypt:   true,
+			Decrypt:   false,
+			Files:     []string{"secrets.geheim.yaml"},
+		},
+	},
+}
+
+func TestCheckEncrypted(t *testing.T) {
+	for _, test := range testCheckEncryptedCases {
+		flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+		args := []string{
+			test.flags.check[0],
+			test.flags.check[1],
+			test.flags.secretKey[0],
+			test.flags.secretKey[1],
+		}
+		os.Args = append([]string{os.Args[0]}, args...)
+		t.Run(test.name, func(t *testing.T) {
+			config := Get()
+			fmt.Println(config)
+			if !compareConfigs(config, test.expected) {
+				t.Logf("Expected: %v, got: %v", test.expected, config)
+				t.Fail()
+			}
+		})
 	}
 }
 
-func TestLocalConfig(t *testing.T) {
-	tests := []Config{{"", "imsosecret", []string{
-		"testfiles/config.json",
-		"testfiles/coverage_testfile.xml",
-		"testfiles/helpers.sh",
-		"testfiles/id_rsa",
-		"testfiles/id_rsa.pub",
-		"testfiles/known_hosts",
-		"testfiles/secrets_test.geheim.yaml",
-		"testfiles/simple.txt",
-		"testfiles/supervisor_env",
-	}, true, false}}
-	for _, test := range tests {
-		config := Get()
-		if !compareConfigs(config, test) {
-			t.Errorf("Get() == %v, expected %v", config, test)
-		}
-	}
-}
+// func init() {
+// 	_, filename, _, _ := runtime.Caller(0)
+// 	dir := path.Join(path.Dir(filename), "../..")
+// 	err := os.Chdir(dir)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
+
+// func TestLocalConfig(t *testing.T) {
+// 	tests := []Config{{"", "imsosecret", []string{
+// 		"testfiles/config.json",
+// 		"testfiles/coverage_testfile.xml",
+// 		"testfiles/helpers.sh",
+// 		"testfiles/id_rsa",
+// 		"testfiles/id_rsa.pub",
+// 		"testfiles/known_hosts",
+// 		"testfiles/secrets_test.geheim.yaml",
+// 		"testfiles/simple.txt",
+// 		"testfiles/supervisor_env",
+// 	}, true, false}}
+// 	for _, test := range tests {
+// 		config := Get()
+// 		if !compareConfigs(config, test) {
+// 			t.Errorf("Get() == %v, expected %v", config, test)
+// 		}
+// 	}
+// }
