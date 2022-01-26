9f4a61b0224374cbe533dbf9b638e410b93ba5a3
doppeldenken <eduardoaemunoz@gmail.com>
2022-01-26 16:02:19 +0000

    refactor tests; main to geheim package

--- geheim_test.go
+++ geheim_test.go
@@ -10,14 +10,20 @@ import (
 )
 
 func TestGeheimEncryptionDecryption(t *testing.T) {
+	t.Parallel()
+
 	tests := testhelpers.GetEncryptionDecryptionTestCases()
 	for i, test := range tests {
+		test := test
+
 		filepath := fmt.Sprintf("testfile.test.%v", i)
 		testhelpers.GenerateTestFiles(test.Testfile, filepath)
 
 		test.Config.Files = []string{filepath}
 
 		t.Run(test.Name, func(t *testing.T) {
+			t.Parallel()
+
 			geheim.Geheim(test.Config)
 
 			if !testhelpers.CheckTestfileResult(filepath, test.Expected) {
@@ -30,15 +36,22 @@ func TestGeheimEncryptionDecryption(t *testing.T) {
 }
 
 func TestGeheimCheck(t *testing.T) {
+	t.Parallel()
+
 	tests := testhelpers.GetCheckTestCases()
 	for i, test := range tests {
+		test := test
+
 		filepath := fmt.Sprintf("testfile.test.%v", i)
 		testhelpers.GenerateTestFiles(test.Testfile, filepath)
 
 		test.Config.Files = []string{filepath}
 
 		t.Run(test.Name, func(t *testing.T) {
+			t.Parallel()
+
 			defer testhelpers.CheckPanic(t, test.Expected)
+
 			geheim.Geheim(test.Config)
 		})
 
